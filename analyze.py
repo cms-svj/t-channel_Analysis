b089521864c66d8bc0db0753609b47eb3dfd6727
Kevin Pedro <kpedro88@gmail.com>
2020-12-17 17:23:06 -0600

    increase timeout, handle waiting, allow changing port

--- analyze.py
+++ analyze.py
@@ -8,11 +8,10 @@ import time
 from optparse import OptionParser
 from glob import glob
 
-def use_dask(njobs):
+def use_dask(njobs,port):
     from dask.distributed import Client
     from lpc_dask.lpc_dask import HTCondorCluster
     import socket
-    import time
 
     # make list of local package directories (absolute paths) that should be sent to jobs
     initpylist = [os.path.abspath(os.path.dirname(x)) for x in glob('*/__init__.py')]
@@ -31,11 +30,14 @@ def use_dask(njobs):
         nanny=False,
         extra=extra,
         job_extra=job_extra,
+        diagnostics_port=port,
     )
 
     cluster.scale(jobs=njobs)
 
-    client = Client(cluster)
+    client = Client(cluster,
+        timeout=100
+    )
 
     exe_args = {
         'client': client,
@@ -58,6 +60,7 @@ def main():
     parser.add_option('-M', '--startFile', help='startFile',         dest='startFile', type=int, default=0)
     parser.add_option(      '--condor',    help='running on condor', dest='condor',              default=False, action='store_true')
     parser.add_option(      '--dask',      help='running on condor w/ dask', dest='dask',              default=False, action='store_true')
+    parser.add_option(      '--port',      help='port for dask status dashboard (localhost:port)', dest='port', type=int, default=8787)
     parser.add_option(      '--quiet',     help='suppress status printouts', dest='quiet',              default=False, action='store_true')
     parser.add_option('-w', '--workers',   help='Number of workers to use for multi-worker executors (e.g. futures or condor)', dest='workers', type=int, default=8)
     parser.add_option('-s', '--chunksize', help='Chunk size',        dest='chunksize', type=int, default=10000)
@@ -74,8 +77,14 @@ def main():
     # get processor args
     exe_args = {'workers': options.workers, 'flatten': False}
     if options.dask:
-        exe_args = use_dask(options.workers)
+        exe_args = use_dask(options.workers,options.port)
         if options.quiet: exe_args['status'] = False
+
+        client = exe_args['client']
+        while len(client.ncores()) < 4:
+            print('Waiting for more cores to spin up, currently there are {0} available...'.format(len(client.ncores())))
+            print('Dask client info ->', client)
+            time.sleep(10)
 
     # run processor
     output = processor.run_uproot_job(
