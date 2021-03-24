a5ebc8303ff6d79bfa5dbbb8a01c9c1a0f0fdbef
Keane-Tan <chin.lung.t93@gmail.com>
2021-03-24 07:47:37 -0500

    Commented out a line in setup.sh

--- setup.sh
+++ setup.sh
@@ -17,7 +17,7 @@ usage(){
 }
 
 NAME=coffeaenv
-LCG=/cvmfs/sft.cern.ch/lcg/views/LCG_96python3/x86_64-centos7-gcc8-opt
+LCG=/cvmfs/sft.cern.ch/lcg/views/LCG_99cuda/x86_64-centos7-gcc8-opt
 DEV=0
 
 # check arguments
@@ -48,50 +48,53 @@ $ECHO "\nMaking and activating the virtual environment ... "
 python -m venv --copies $NAME
 source $NAME/bin/activate
 
-$ECHO "\nSetup for Dask on LPC ... "
-pypackages=lib/python3.6/site-packages/
-lcgprefix=${LCG}/${pypackages}
-# need to remove python path from LCG to avoid dask conflicts
-export PYTHONPATH=""
-ln -sf ${lcgprefix}/pyxrootd ${NAME}/${pypackages}/pyxrootd
-ln -sf ${lcgprefix}/XRootD ${NAME}/${pypackages}/XRootD
-git clone git@github.com:cms-svj/lpc_dask
-python -m pip install --no-cache-dir dask[dataframe]==2020.12.0 distributed==2020.12.0 dask-jobqueue
+#$ECHO "\nSetup for Dask on LPC ... "
+#pypackages=lib/python3.7/site-packages/
+#lcgprefix=${LCG}/${pypackages}
+## need to remove python path from LCG to avoid dask conflicts
+#export PYTHONPATH=""
+#ln -sf ${lcgprefix}/pyxrootd ${NAME}/${pypackages}/pyxrootd
+#ln -sf ${lcgprefix}/XRootD ${NAME}/${pypackages}/XRootD
+#git clone git@github.com:cms-svj/lpc_dask
+#python -m pip install --no-cache-dir dask[dataframe]==2020.12.0 distributed==2020.12.0 dask-jobqueue
 
 $ECHO "\nInstalling 'pip' packages ... "
-python -m pip install --no-cache-dir setuptools pip wheel --upgrade
+python -m pip install --no-cache-dir setuptools pip argparse --upgrade
 python -m pip install --no-cache-dir xxhash
 python -m pip install --no-cache-dir uproot4
+python -m pip install --no-cache-dir magiconfig
 if [[ "$DEV" == "1" ]]; then
 	$ECHO "\nInstalling the 'development' version of Coffea ... "
 	python -m pip install --no-cache-dir flake8 pytest coverage
 	git clone https://github.com/CoffeaTeam/coffea
 	cd coffea
-	python -m pip install --no-cache-dir --editable .[dask,spark,parsl]
+	python -m pip install --no-cache-dir --editable .[dask,spark,parsl] 'pillow>=7.1.0'
 	cd ..
 else
 	$ECHO "Installing the 'production' version of Coffea ... "
-	python -m pip install --no-cache-dir coffea[dask,spark,parsl]==0.6.47
+	python -m pip install --no-cache-dir coffea[dask,spark,parsl] 'pillow>=7.1.0'
 fi
 
-# apply patches
-./patch.sh $NAME
-
-# Clone TreeMaker for its lists of samples and files
-$ECHO "\nCloning the TreeMaker repository ..."
-git clone git@github.com:TreeMaker/TreeMaker.git ${NAME}/${pypackages}/TreeMaker/
+## apply patches
+#./patch.sh $NAME
+#
+## Clone TreeMaker for its lists of samples and files
+#$ECHO "\nCloning the TreeMaker repository ..."
+#git clone git@github.com:TreeMaker/TreeMaker.git ${NAME}/${pypackages}/TreeMaker/
 
 # Setup the activation script for the virtual environment
 $ECHO "\nSetting up the activation script for the virtual environment ... "
 sed -i '40s/.*/VIRTUAL_ENV="$(cd "$(dirname "$(dirname "${BASH_SOURCE[0]}" )")" \&\& pwd)"/' $NAME/bin/activate
-find coffeaenv/bin/ -type f -print0 | xargs -0 -P 4 sed -i '1s/#!.*python$/#!\/usr\/bin\/env python/'
-sed -i "2a source ${LCG}/setup.sh"'\nexport PYTHONPATH=""' $NAME/bin/activate
-sed -i "4a source ${LCG}/setup.csh"'\nsetenv PYTHONPATH ""' $NAME/bin/activate.csh
+find ${NAME}/bin/ -type f -print0 | xargs -0 -P 4 sed -i '1s/#!.*python$/#!\/usr\/bin\/env python/'
+#sed -i "2a source ${LCG}/setup.sh"'\nexport PYTHONPATH=""' $NAME/bin/activate
+#sed -i "4a source ${LCG}/setup.csh"'\nsetenv PYTHONPATH ""' $NAME/bin/activate.csh
+sed -i "2a source ${LCG}/setup.sh" $NAME/bin/activate
+sed -i "4a source ${LCG}/setup.csh" $NAME/bin/activate.csh
 
-$ECHO "\nSetting up the ipython/jupyter kernel ... "
-storage_dir=$(readlink -f $PWD)
-ipython kernel install --prefix=${storage_dir}/.local --name=$NAME
-tar -zcf ${NAME}.tar.gz ${NAME}
+#$ECHO "\nSetting up the ipython/jupyter kernel ... "
+#storage_dir=$(readlink -f $PWD)
+#ipython kernel install --prefix=${storage_dir}/.local --name=$NAME
+#tar -zcf ${NAME}.tar.gz ${NAME}
 
 deactivate
 $ECHO "\nFINISHED"
