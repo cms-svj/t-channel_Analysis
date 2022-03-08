a45ba186b2cbe8a582d5fedb472a5bf51eb1d243
Keane-Tan <chin.lung.t93@gmail.com>
2022-03-08 17:51:58 -0600

    Made the trigger requirement more flexible

--- utils/baseline.py
+++ utils/baseline.py
@@ -1,6 +1,80 @@
 import numpy as np
 import awkward as ak
 
+# UL2018 Ntuples Good triggers
+trigDict = {
+'HLT_AK8PFHT800_TrimMass50_v': 4,
+'HLT_AK8PFHT850_TrimMass50_v': 5,
+'HLT_AK8PFHT900_TrimMass50_v': 6,
+'HLT_AK8PFJet400_TrimMass30_v': 8,
+'HLT_AK8PFJet420_TrimMass30_v': 9,
+'HLT_AK8PFJet500_v': 11,
+'HLT_AK8PFJet550_v': 12,
+'HLT_AK8PFJetFwd400_v': 13,
+'HLT_CaloJet500_NoJetID_v': 14,
+'HLT_CaloJet550_NoJetID_v': 15,
+'HLT_CaloMET350_HBHECleaned_v': 16,
+'HLT_DiPFJetAve300_HFJEC_v': 21,
+'HLT_DoubleEle8_CaloIdM_TrackIdM_Mass8_DZ_PFHT350_v': 22,
+'HLT_DoubleEle8_CaloIdM_TrackIdM_Mass8_PFHT350_v': 25,
+'HLT_Ele115_CaloIdVT_GsfTrkIdT_v': 31,
+'HLT_Ele135_CaloIdVT_GsfTrkIdT_v': 32,
+'HLT_Ele145_CaloIdVT_GsfTrkIdT_v': 33,
+'HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v': 43,
+'HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_v': 44,
+'HLT_Ele28_eta2p1_WPTight_Gsf_HT150_v': 48,
+'HLT_Ele30_eta2p1_WPTight_Gsf_CentralPFJet35_EleCleaned_v': 49,
+'HLT_Ele32_WPTight_Gsf_v': 50,
+'HLT_Ele35_WPTight_Gsf_v': 51,
+'HLT_IsoMu27_v': 71,
+'HLT_MonoCentralPFJet80_PFMETNoMu120_PFMHTNoMu120_IDTight_v': 76,
+'HLT_MonoCentralPFJet80_PFMETNoMu130_PFMHTNoMu130_IDTight_v': 77,
+'HLT_MonoCentralPFJet80_PFMETNoMu140_PFMHTNoMu140_IDTight_v': 78,
+'HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v': 79,
+'HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8_v': 88,
+'HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8_v': 89,
+'HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v': 94,
+'HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v': 95,
+'HLT_Mu50_v': 101,
+'HLT_Mu55_v': 102,
+'HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v': 107,
+'HLT_PFHT1050_v': 108,
+'HLT_PFHT330PT30_QuadPFJet_75_60_45_40_TriplePFBTagDeepCSV_4p5_v': 115,
+'HLT_PFHT500_PFMET100_PFMHT100_IDTight_v': 139,
+'HLT_PFHT500_PFMET110_PFMHT110_IDTight_v': 140,
+'HLT_PFHT700_PFMET85_PFMHT85_IDTight_v': 148,
+'HLT_PFHT700_PFMET95_PFMHT95_IDTight_v': 149,
+'HLT_PFHT800_PFMET75_PFMHT75_IDTight_v': 154,
+'HLT_PFHT800_PFMET85_PFMHT85_IDTight_v': 155,
+'HLT_PFJet500_v': 160,
+'HLT_PFJet550_v': 161,
+'HLT_PFJetFwd400_v': 162,
+'HLT_PFMET120_PFMHT120_IDTight_v': 170,
+'HLT_PFMET130_PFMHT130_IDTight_v': 172,
+'HLT_PFMET140_PFMHT140_IDTight_v': 174,
+'HLT_PFMET200_HBHE_BeamHaloCleaned_v': 176,
+'HLT_PFMET250_HBHECleaned_v': 177,
+'HLT_PFMET300_HBHECleaned_v': 178,
+'HLT_PFMETNoMu120_PFMHTNoMu120_IDTight_v': 190,
+'HLT_PFMETNoMu130_PFMHTNoMu130_IDTight_v': 192,
+'HLT_PFMETNoMu140_PFMHTNoMu140_IDTight_v': 194,
+'HLT_PFMETTypeOne140_PFMHT140_IDTight_v': 198,
+'HLT_PFMETTypeOne200_HBHE_BeamHaloCleaned_v': 199,
+'HLT_Photon200_v': 204,
+'HLT_Photon300_NoHE_v': 205,
+'HLT_TkMu100_v': 209
+}
+
+schTriggers = {
+'HLT_AK8PFJet500_v':11,
+'HLT_AK8PFJet550_v':12,
+'HLT_CaloJet500_NoJetID_v':14,
+'HLT_CaloJet550_NoJetID_v':15,
+'HLT_PFHT1050_v':108,
+'HLT_PFJet500_v':160,
+'HLT_PFJet550_v':161,
+}
+
 def TTStitch(dataset,events):
     # # TT Stiching mask
     madHT = events.madHT
@@ -82,13 +156,18 @@ def METFilters(events):
 def Preselection(qualityCuts,nl):
     return (qualityCuts & (nl == 0))
 
-def PassTrigger(triggerPass):
-    indicesOfHighEffTrig = [11,12,13,14,67,107,108,131,8,90,98,116]
+def trgListtoInd(trgList):
+    indList = []
+    for trg in trgList:
+        indList.append(trigDict["HLT_{}_v".format(trg)])
+    return indList
+
+def PassTrigger(triggerPass,indices):
     triggerPass = ak.to_numpy(triggerPass)
     nTrigs= len(triggerPass[0])
     trigReq = []
     for i in range(nTrigs):
-        if i in indicesOfHighEffTrig:
+        if i in indices:
             trigReq.append(1)
         else:
             trigReq.append(0)
@@ -111,7 +190,6 @@ def cutList(dataset,events,vars_noCut,SVJCut=True):
     jetIDAK8 = events.JetIDAK8
     ttStitch = TTStitch(dataset,events)
     metFilters = METFilters(events)
-    triggerCut = PassTrigger(triggerPass)
     # psFilter = PhiSpikeFilter(dataset,vars_noCut['jets'])
     # qualityCuts = metFilters & psFilter & ttStitch
     # qualityCuts = metFilters & psFilter # NN training files
@@ -123,12 +201,52 @@ def cutList(dataset,events,vars_noCut,SVJCut=True):
 
     cuts = {
             ""                          : np.ones(len(evtw),dtype=bool),
-            "_trg"                         : triggerCut,
-            "_trg_metfilter"             : triggerCut & metFilters,
             "_metfilter_0l"              : metFilters & (nl == 0),
-            "_trg_metfilter_0l"          : triggerCut & metFilters & (nl == 0),
-            # "_trg_metfilter_0l_trgPlat"  : triggerCut & metFilters & (nl == 0) & trgPlat,
     }
+
+    # trigger choices
+    HETrg_noSch = [
+        "AK8PFHT800_TrimMass50",
+        "AK8PFJet400_TrimMass30",
+        "AK8PFJet500",
+        "AK8PFJetFwd400",
+        "CaloJet500_NoJetID",
+        "DiPFJetAve300_HFJEC",
+        "PFHT1050",
+        "PFHT330PT30_QuadPFJet_75_60_45_40_TriplePFBTagDeepCSV_4p5",
+        "PFHT500_PFMET100_PFMHT100_IDTight",
+        "PFHT700_PFMET85_PFMHT85_IDTight",
+        "PFHT800_PFMET75_PFMHT75_IDTight",
+        "PFMET120_PFMHT120_IDTight",
+        "PFMETNoMu120_PFMHTNoMu120_IDTight",
+        "PFMETTypeOne140_PFMHT140_IDTight",
+        "PFMETTypeOne200_HBHE_BeamHaloCleaned"
+    ]
+    HETrg_noSch_ind =  trgListtoInd(HETrg_noSch)
+    cuts["_HETrg_noSch"] = PassTrigger(triggerPass,HETrg_noSch_ind)
+    HETrg_wSch = [
+        "AK8PFHT800_TrimMass50",
+        "AK8PFJet400_TrimMass30",
+        "PFHT330PT30_QuadPFJet_75_60_45_40_TriplePFBTagDeepCSV_4p5",
+        "PFHT500_PFMET100_PFMHT100_IDTight",
+        "PFHT700_PFMET85_PFMHT85_IDTight",
+        "PFHT800_PFMET75_PFMHT75_IDTight",
+        "PFMET120_PFMHT120_IDTight",
+        "PFMETNoMu120_PFMHTNoMu120_IDTight",
+        "PFMETTypeOne200_HBHE_BeamHaloCleaned"
+    ]
+    HETrg_wSch_ind =  trgListtoInd(HETrg_wSch)
+    schTrigIns = list(schTriggers.values())
+    cuts["_HETrg_wSch"] = PassTrigger(triggerPass,HETrg_wSch_ind + schTrigIns)
+    oldHEsch = [
+        "PFMETNoMu120_PFMHTNoMu120_IDTight",
+        "AK8PFJet400_TrimMass30",
+        "PFHT500_PFMET100_PFMHT100_IDTight",
+        "PFHT700_PFMET85_PFMHT85_IDTight",
+        "PFMET120_PFMHT120_IDTight",
+    ]
+    oldHEsch_ind =  trgListtoInd(oldHEsch)
+    cuts["_oldHEsch"] = PassTrigger(triggerPass,oldHEsch_ind + schTrigIns)
     # cuts with svj
     if SVJCut == True:
         nsvjJetsAK8 = vars_noCut["nsvjJetsAK8"][0]
