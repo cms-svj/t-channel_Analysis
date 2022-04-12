b6308182f1629deb3e50d610cb77160bea22b5ff
Christopher_Madrid <ChrisMadrid173@gmail.com>
2022-04-12 12:11:41 -0500

    Removing changes that broke condor env

--- init.sh
+++ init.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-NAME=coffeaenv
-
-if [[ "$1" != "" ]]; then
-    NAME=$1
-fi
-
 # vars for jupyter
 storage_dir=$(readlink -f $PWD)
 export TCHANNEL_BASE=${storage_dir}
@@ -14,6 +8,4 @@ export JUPYTER_RUNTIME_DIR=${storage_dir}/.local/share/jupyter/runtime
 export JUPYTER_DATA_DIR=${storage_dir}/.local/share/jupyter
 export IPYTHONDIR=${storage_dir}/.ipython
 
-# source virtual environment
-source $NAME/bin/activate
-
+source coffeaenv/bin/activate
