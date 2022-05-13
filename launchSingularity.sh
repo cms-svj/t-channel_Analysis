b30c7400fd291bc7cf80c569e2be19719b911f01
Kevin Pedro <kpedro88@gmail.com>
2022-05-13 14:15:37 -0500

    use SINGULARITY_BIND to make more independent of lpc

--- launchSingularity.sh
+++ launchSingularity.sh
@@ -1,1 +1,9 @@
-singularity run --nv --bind `readlink $HOME` --bind /uscms_data --bind `readlink -f ${HOME}/nobackup/` --bind /cvmfs /cvmfs/unpacked.cern.ch/registry.hub.docker.com/fnallpc/fnallpc-docker:pytorch-1.9.0-cuda11.1-cudnn8-runtime-singularity
+#!/bin/bash
+
+if [ -z "$SINGULARITY_BIND" ]; then
+	if [[ "$(uname -a)" == *cms*.fnal.gov* ]]; then
+		export SINGULARITY_BIND="$(readlink -f $HOME),/uscms_data,$(readlink -f ${HOME}/nobackup/)"
+	fi
+fi
+
+singularity run --nv --bind /cvmfs /cvmfs/unpacked.cern.ch/registry.hub.docker.com/fnallpc/fnallpc-docker:pytorch-1.9.0-cuda11.1-cudnn8-runtime-singularity
