ab4a856ea58cae5c9b795ccfa0cc08fcee53a6f2
Christopher_Madrid <ChrisMadrid173@gmail.com>
2022-06-17 11:59:20 -0500

    Adding more plots

--- plotFakerate.py
+++ plotFakerate.py
@@ -20,11 +20,11 @@ def getHisto(f, name, color=None, lw=None, ls=None, ms=None, scale=None, fill=No
 
 def cleanHistos(num, den):
     nXBins = den.GetXaxis().GetNbins()
-    for i in range(0, nXBins+1):
+    for i in range(0, nXBins+2):
         n = num.GetBinContent(i)
         d = den.GetBinContent(i)
-        if(d == 0.0 and n > 0.0):
-            print(n, d)
+        if(n>d):
+            print(i, n, d)
             num.SetBinContent(i, 0.0)
             den.SetBinContent(i, 0.0)
     return num, den
@@ -32,7 +32,7 @@ def cleanHistos(num, den):
 def drawPlots(f, suffix1, suffix2, scale=1.0, drawName="", rebin=None):
     num = getHisto(f, "h_{}".format(suffix1), ROOT.kBlack,  3, ROOT.kDotted, None, None, ROOT.kAzure+1)
     den = getHisto(f, "h_{}".format(suffix2), ROOT.kBlack , 3, 1)
-    #num, den = cleanHistos(num,den)
+    num, den = cleanHistos(num,den)
     if rebin:
         num.Rebin(rebin)
         den.Rebin(rebin)
@@ -74,22 +74,25 @@ def drawPlots(f, suffix1, suffix2, scale=1.0, drawName="", rebin=None):
 
 def drawSummaryPlot(name, hList):
     canvas = ROOT.TCanvas("cv","cv",1000,800)
-    colors = [ROOT.kRed, ROOT.kBlack, ROOT.kBlue, ROOT.kGreen+2]
+    #ROOT.gPad.SetLogy()    
+    colors = [ROOT.kRed, ROOT.kBlack, ROOT.kBlue, ROOT.kGreen+2, ROOT.kCyan, ROOT.kRed+2, ROOT.kBlack-2, ROOT.kBlue+2, ROOT.kGreen-2, ROOT.kCyan+2]
 
     hBase = hList[0][0].GetHistogram()
     hBase.SetMaximum(1.5)
+    hBase.SetTitle("")
     hBase.Draw()
 
-    legend = ROOT.TLegend(0.15,0.65,0.45,0.85)
+    legend = ROOT.TLegend(0.55,0.65,0.95,0.90)
     line = ROOT.TF1("line","1.0",-5000.0,5000.0)
     line.SetLineColor(ROOT.kBlack)
     
-    for i, t in enumerate(hList):
-        h , hT = t
-        h.SetLineColor(colors[i])
-        hT.SetLineColor(colors[i])
-        h.SetMarkerColor(colors[i])
-        hT.SetMarkerColor(colors[i])
+    for h , hT, color, s in hList:
+        h.SetLineColor(color)
+        hT.SetLineColor(color)
+        if s: h.SetLineStyle(s)
+        if s: hT.SetLineStyle(s)
+        h.SetMarkerColor(color)
+        hT.SetMarkerColor(color)
         legend.AddEntry(h, h.GetName(),"l")
         h.Draw("same")
         #hT.Draw("same")
@@ -102,21 +105,36 @@ def drawSummaryPlot(name, hList):
 
 
 def main():    
-    f = ROOT.TFile("test.root")
+    #f = ROOT.TFile("test.root")
+    f = ROOT.TFile("2018_QCD.root")
     hList = {"Pt_":[],"Eta":[]}
-    hList["Pt_"].append(drawPlots(f, "svjPtAK8_qual_trg_st",  "jPtAK8_qual_trg_st",  1.0, "jPt_Fakerate",  10))
-    hList["Eta"].append(drawPlots(f, "svjEtaAK8_qual_trg_st", "jEtaAK8_qual_trg_st", 1.0, "jEta_Fakerate", 10))
+    hList["Pt_"].append(drawPlots(f, "svjPtAK8_qual_trg_st_0nim",         "jPtAK8_qual_trg_st_0nim",         1.0, "jPt_Fakerate_SR",      5) + (ROOT.kRed,     None)        )
+    hList["Pt_"].append(drawPlots(f, "svjPtAK8_qual_trg_st_0nim_1J",      "jPtAK8_qual_trg_st_0nim_1J",      1.0, "jPt_Fakerate_SR_1J",   5) + (ROOT.kGreen+2, None)        )
+    hList["Pt_"].append(drawPlots(f, "svjPtAK8_qual_trg_st_0nim_ge1J",    "jPtAK8_qual_trg_st_0nim_ge1J",    1.0, "jPt_Fakerate_SR_ge1J", 5) + (ROOT.kBlack,   None)        )
+    hList["Pt_"].append(drawPlots(f, "svjPtAK8_qual_trg_st_0nim_2J",      "jPtAK8_qual_trg_st_0nim_2J",      1.0, "jPt_Fakerate_SR_2J",   5) + (ROOT.kBlue+1,  None)        )
+    hList["Pt_"].append(drawPlots(f, "svjPtAK8_qual_trg_st_0nim_ge2J",    "jPtAK8_qual_trg_st_0nim_ge2J",    1.0, "jPt_Fakerate_SR_ge2J", 5) + (ROOT.kCyan,    None)        )
+    hList["Pt_"].append(drawPlots(f, "svjPtAK8_qual_trg_st_ge1nim",       "jPtAK8_qual_trg_st_ge1nim",       1.0, "jPt_Fakerate_CR",      5) + (ROOT.kRed,     ROOT.kDashed))
+    hList["Pt_"].append(drawPlots(f, "svjPtAK8_qual_trg_st_ge1nim_1J",    "jPtAK8_qual_trg_st_ge1nim_1J",    1.0, "jPt_Fakerate_CR_1J",   5) + (ROOT.kGreen+2, ROOT.kDashed))
+    hList["Pt_"].append(drawPlots(f, "svjPtAK8_qual_trg_st_ge1nim_ge1J",  "jPtAK8_qual_trg_st_ge1nim_ge1J",  1.0, "jPt_Fakerate_CR_ge1J", 5) + (ROOT.kBlack,   ROOT.kDashed))
+    hList["Pt_"].append(drawPlots(f, "svjPtAK8_qual_trg_st_ge1nim_2J",    "jPtAK8_qual_trg_st_ge1nim_2J",    1.0, "jPt_Fakerate_CR_2J",   5) + (ROOT.kBlue+1,  ROOT.kDashed))
+    hList["Pt_"].append(drawPlots(f, "svjPtAK8_qual_trg_st_ge1nim_ge2J",  "jPtAK8_qual_trg_st_ge1nim_ge2J",  1.0, "jPt_Fakerate_CR_ge2J", 5) + (ROOT.kCyan,    ROOT.kDashed))
 
-    hList["Pt_"].append(drawPlots(f, "svjPtAK8_qual_trg_st_0nim",  "jPtAK8_qual_trg_st_0nim",  1.0, "jPt_Fakerate_SR",  10))
-    hList["Eta"].append(drawPlots(f, "svjEtaAK8_qual_trg_st_0nim", "jEtaAK8_qual_trg_st_0nim", 1.0, "jEta_Fakerate_SR", 10))
+    hList["Eta"].append(drawPlots(f, "svjEtaAK8_qual_trg_st_0nim",        "jEtaAK8_qual_trg_st_0nim",        1.0, "jEta_Fakerate_SR",     5) + (ROOT.kRed,     None)        )
+    hList["Eta"].append(drawPlots(f, "svjEtaAK8_qual_trg_st_0nim_1J",     "jEtaAK8_qual_trg_st_0nim_1J",     1.0, "jEta_Fakerate_SR_1J",  5) + (ROOT.kGreen+2, None)        )
+    hList["Eta"].append(drawPlots(f, "svjEtaAK8_qual_trg_st_0nim_ge1J",   "jEtaAK8_qual_trg_st_0nim_ge1J",   1.0, "jEta_Fakerate_SR_ge1J",5) + (ROOT.kBlack,   None)        )
+    hList["Eta"].append(drawPlots(f, "svjEtaAK8_qual_trg_st_0nim_2J",     "jEtaAK8_qual_trg_st_0nim_2J",     1.0, "jEta_Fakerate_SR_2J",  5) + (ROOT.kBlue+1,  None)        )
+    hList["Eta"].append(drawPlots(f, "svjEtaAK8_qual_trg_st_0nim_ge2J",   "jEtaAK8_qual_trg_st_0nim_ge2J",   1.0, "jEta_Fakerate_SR_ge2J",5) + (ROOT.kCyan,    None)        )
+    hList["Eta"].append(drawPlots(f, "svjEtaAK8_qual_trg_st_ge1nim",      "jEtaAK8_qual_trg_st_ge1nim",      1.0, "jEta_Fakerate_CR",     5) + (ROOT.kRed,     ROOT.kDashed))
+    hList["Eta"].append(drawPlots(f, "svjEtaAK8_qual_trg_st_ge1nim_1J",   "jEtaAK8_qual_trg_st_ge1nim_1J",   1.0, "jEta_Fakerate_CR_1J",  5) + (ROOT.kGreen+2, ROOT.kDashed))
+    hList["Eta"].append(drawPlots(f, "svjEtaAK8_qual_trg_st_ge1nim_ge1J", "jEtaAK8_qual_trg_st_ge1nim_ge1J", 1.0, "jEta_Fakerate_CR_ge1J",5) + (ROOT.kBlack,   ROOT.kDashed))
+    hList["Eta"].append(drawPlots(f, "svjEtaAK8_qual_trg_st_ge1nim_2J",   "jEtaAK8_qual_trg_st_ge1nim_2J",   1.0, "jEta_Fakerate_CR_2J",  5) + (ROOT.kBlue+1,  ROOT.kDashed))
+    hList["Eta"].append(drawPlots(f, "svjEtaAK8_qual_trg_st_ge1nim_ge2J", "jEtaAK8_qual_trg_st_ge1nim_ge2J", 1.0, "jEta_Fakerate_CR_ge2J",5) + (ROOT.kCyan,    ROOT.kDashed))
 
-    hList["Pt_"].append(drawPlots(f, "svjPtAK8_qual_trg_st_ge1nim",  "jPtAK8_qual_trg_st_ge1nim",  1.0, "jPt_Fakerate_CR",  10))
-    hList["Eta"].append(drawPlots(f, "svjEtaAK8_qual_trg_st_ge1nim", "jEtaAK8_qual_trg_st_ge1nim", 1.0, "jEta_Fakerate_CR", 10))
 
     outfile = ROOT.TFile("fakerate.root","RECREATE")
     for n,l in hList.items():
         drawSummaryPlot(n, l)
-        for h, hT in l:
+        for h, hT, _, _ in l:
             h.Write()
     outfile.Close()
 
--- plotFakerateBackground.py
+++ plotFakerateBackground.py
@@ -1,6 +1,54 @@
 import ROOT
 
 ROOT.gROOT.SetBatch( True )
+ROOT.gStyle.SetOptStat("")
+ROOT.gStyle.SetPaintTextFormat("3.2f")
+ROOT.gStyle.SetFrameLineWidth(2)
+ROOT.gStyle.SetEndErrorSize(0)
+ROOT.TH1.SetDefaultSumw2()
+ROOT.TH2.SetDefaultSumw2()
+
+# Create a canvas and determine if it should be split for a ratio plot
+# Margins are scaled on-the-fly so that distances are the same in either
+# scenario.
+def makeCanvas(doLogY):
+
+    canvas = ROOT.TCanvas("cv", "cv", 1000, 800)
+
+    # Split the canvas 70 / 30 by default if doing ratio
+    # scale parameter keeps text sizes in ratio panel the
+    # same as in the upper panel
+    split      = 0.3
+    upperSplit = 1.0
+    lowerSplit = 1.0
+    scale      = 1.0
+    TopMargin    = 0.06
+    BottomMargin = 0.12
+    RightMargin  = 0.04
+    LeftMargin   = 0.16
+    upperSplit = 1.0-split
+    lowerSplit = split
+    scale = upperSplit / lowerSplit
+
+    canvas.Divide(1,2)
+
+    canvas.cd(1)
+    ROOT.gPad.SetPad(0.0, split, 1.0, 1.0)
+    ROOT.gPad.SetTopMargin(TopMargin / upperSplit)
+    ROOT.gPad.SetBottomMargin(0)
+    ROOT.gPad.SetLeftMargin(LeftMargin)
+    ROOT.gPad.SetRightMargin(RightMargin)
+    if doLogY:
+        ROOT.gPad.SetLogy()
+
+    canvas.cd(2)
+    ROOT.gPad.SetPad(0.0, 0.0, 1.0, split)
+    ROOT.gPad.SetTopMargin(0)
+    ROOT.gPad.SetBottomMargin(BottomMargin / lowerSplit)
+    ROOT.gPad.SetLeftMargin(LeftMargin)
+    ROOT.gPad.SetRightMargin(RightMargin)
+
+    return canvas
 
 def getHisto(f, name, color=None, lw=None, ls=None, ms=None, scale=None):
     h = f.Get(name)
@@ -9,6 +57,8 @@ def getHisto(f, name, color=None, lw=None, ls=None, ms=None, scale=None):
     if ls:    h.SetLineStyle(ls)
     if ms:    h.SetMarkerStyle(ms)
     if scale: h.Scale(scale)
+    h.GetYaxis().SetTitle("Events")
+    h.GetYaxis().SetLabelSize(0.05)
     return h
 
 def drawPlots(f, suffix1, suffix2, scale=1.0, drawName=""):
@@ -17,31 +67,67 @@ def drawPlots(f, suffix1, suffix2, scale=1.0, drawName=""):
     nsvjJetsAK8Plus2 = getHisto(f, "h_nsvjJetsAK8Plus2_qual_trg_st_{}".format(suffix1+suffix2), ROOT.kBlack, 3, 1,    ROOT.kFullDotLarge, scale)
     nsvjJetsAK8Plus3 = getHisto(f, "h_nsvjJetsAK8Plus3_qual_trg_st_{}".format(suffix1+suffix2), ROOT.kBlack, 3, 1,    ROOT.kFullDotLarge, scale)
     nsvjJetsAK8Plus4 = getHisto(f, "h_nsvjJetsAK8Plus4_qual_trg_st_{}".format(suffix1+suffix2), ROOT.kBlack, 3, 1,    ROOT.kFullDotLarge, scale)
+    nsvjJetsAK8Plus1.Add(nsvjJetsAK8Plus2)
+    nsvjJetsAK8Plus1.Add(nsvjJetsAK8Plus3)
+    nsvjJetsAK8Plus1.Add(nsvjJetsAK8Plus4)
+    nsvjJetsAK8Pred = nsvjJetsAK8Plus1
 
-    canvas = ROOT.TCanvas("cv","cv",1000,800)
-    #ROOT.gPad.SetLogy()
-    nsvjJetsAK8.SetMinimum(1e-1)    
-    nsvjJetsAK8.SetMaximum(1e4)
+    ratio = nsvjJetsAK8.Clone("ratio")
+    ratio.SetTitle("")
+    ratio.GetYaxis().SetTitle("True/Pred.")
+    ratio.Divide(nsvjJetsAK8Pred)
+    ratio.SetLineColor(ROOT.kBlack)
+    ratio.SetLineWidth(3)
+    ratio.SetLineStyle(1)
+    ratio.SetMarkerStyle(ROOT.kFullDotLarge)
+    ratio.GetXaxis().SetLabelSize(0.08)
+    ratio.GetYaxis().SetLabelSize(0.08)
+    ratio.GetXaxis().SetTitleSize(0.08)
+    ratio.GetYaxis().SetTitleSize(0.08)
+    ratio.SetMaximum(21.0)
 
+    #fit = ROOT.TF1('fit','[0]*TMath::Poisson(x-[1],[2])',1+2,5+2)
+    #fit.SetLineColor(ROOT.kRed)
+    #fit.SetParameter(0,100.0)
+    #fit.SetParameter(1, -1)
+    #fit.SetParameter(2, 10.0)
+    #ratio.Fit(fit,"Q", "", 1+2, 5+2)
 
+    canvas = makeCanvas(doLogY=True)
+    ROOT.gStyle.SetOptStat(0)
+    nsvjJetsAK8.SetMinimum(1.5e-2)    
+    nsvjJetsAK8.SetMaximum(1e8)
+
+    canvas.cd(1)
     nsvjJetsAK8.Draw("hist E")
-    nsvjJetsAK8Plus1.Draw("PEX0 same")
-    nsvjJetsAK8Plus2.Draw("PEX0 same")
-    nsvjJetsAK8Plus3.Draw("PEX0 same")
-    nsvjJetsAK8Plus4.Draw("PEX0 same")
+    nsvjJetsAK8Pred.Draw("PEX0 same")
+
+    canvas.cd(2)
+    ROOT.gPad.SetGridy()
+    ratio.Draw("E0P")
+    #fit.Draw("same")
 
     canvas.SaveAs("fakerateBGEstimation{}{}.gif".format(suffix2, drawName))
     canvas.SaveAs("fakerateBGEstimation{}{}.pdf".format(suffix2, drawName))
 
 def main():    
-    f = ROOT.TFile("test.root")
-    drawPlots(f, "0nim", "_0J", 5.5, "SR")
-    drawPlots(f, "0nim", "_1J", 2.1, "SR")
-    drawPlots(f, "0nim", "_2J", 1.15, "SR")
+    f = ROOT.TFile("2018_QCD.root")
+    drawPlots(f, "0nim", "_0J", 1.3, "SR")
+    drawPlots(f, "0nim", "_1J", 1.1, "SR")
+    drawPlots(f, "0nim", "_2J", 1.1, "SR")
 
-    drawPlots(f, "ge1nim", "_0J", 17.0,"CR")
-    drawPlots(f, "ge1nim", "_1J", 4.2 ,"CR")
-    drawPlots(f, "ge1nim", "_2J", 1.2, "CR")
+    drawPlots(f, "ge1nim", "_0J", 2.5,"CR")
+    drawPlots(f, "ge1nim", "_1J", 2.0 ,"CR")
+    drawPlots(f, "ge1nim", "_2J", 1.5, "CR")
+
+    #f = ROOT.TFile("test.root")
+    #drawPlots(f, "0nim", "_0J", 5.5, "SR")
+    #drawPlots(f, "0nim", "_1J", 2.1, "SR")
+    #drawPlots(f, "0nim", "_2J", 1.15, "SR")
+
+    #drawPlots(f, "ge1nim", "_0J", 17.0,"CR")
+    #drawPlots(f, "ge1nim", "_1J", 4.2 ,"CR")
+    #drawPlots(f, "ge1nim", "_2J", 1.2, "CR")
 
 if __name__ == '__main__':
     main()
