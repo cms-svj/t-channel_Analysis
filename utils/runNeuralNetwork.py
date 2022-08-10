a791cf8db412625edbee4a5ce21f2290f2b927a8
Christopher_Madrid <ChrisMadrid173@gmail.com>
2022-08-10 11:38:59 -0500

    Adding fakerate checks

--- utils/runNeuralNetwork.py
+++ utils/runNeuralNetwork.py
@@ -93,7 +93,7 @@ def findFakeRate(fakerateHisto, bgroundJetsAK8):
 def getFlatScore(nnOutput):
     output = ak.to_list(ak.zeros_like(nnOutput))
     for i, a in enumerate(nnOutput):
-        output[i] = random.uniform(0, 1)
+        output[i] = random.uniform(0.5, 1)
     return output
 
 def runNN(model,varsIn,varSet,normMean,normStd,fakerateHisto):
@@ -103,6 +103,7 @@ def runNN(model,varsIn,varSet,normMean,normStd,fakerateHisto):
     fjets = varsIn["fjets"]
     counts = ak.num(fjets.pt)
     svjJetsAK8 = ak.unflatten(nnOutput, counts)
+    #svjJetsAK8 = np.exp(-fjets.pt/5000.0)*svjJetsAK8    
 
     wpt = 0.7
     darksvjJetsAK8 = fjets[svjJetsAK8 >= wpt]
