1389b17760ebeb4cc0eb1f698c845d7c9c98a683
Keane-Tan <chin.lung.t93@gmail.com>
2023-06-26 17:18:53 -0500

    Update analyze_root_module to share the same dask setup as analyze

--- analyze_root_varModule.py
+++ analyze_root_varModule.py
@@ -8,50 +8,7 @@ import time
 from optparse import OptionParser
 from glob import glob
 import numpy as np
-
-def use_dask(condor,njobs,port):
-    from dask.distributed import Client
-    from lpc_dask.lpc_dask import HTCondorCluster
-    import socket
-
-    # make list of local package directories (absolute paths) that should be sent to jobs
-    initpylist = [os.path.abspath(os.path.dirname(x)) for x in glob('*/__init__.py')]
-    initpylist.append("patch.sh")
-    job_extra = {'transfer_input_files': ','.join(initpylist)}
-
-    extra = ['--worker-port 10002:10100']
-
-    hostname = socket.gethostname()
-
-    if condor:
-        cluster = HTCondorCluster(
-            scheduler_options = {'host': f'{hostname}:10000', 'dashboard_address': ':{}'.format(port)},
-            cores=1,
-            memory="2GB",
-            disk="2GB",
-            python='python',
-            nanny=False,
-            extra=extra,
-            job_extra=job_extra,
-        )
-
-        cluster.scale(jobs=njobs)
-
-        client = Client(cluster,
-            timeout=100
-        )
-    else:
-        client = Client()
-
-    exe_args = {
-        'client': client,
-        'savemetrics': True,
-        'schema': None,
-        'nano': False,
-        'align_clusters': True
-    }
-
-    return exe_args
+from utils.python.lpc_dask import *
 
 def main():
     # start run time clock
@@ -71,6 +28,9 @@ def main():
     parser.add_option('-w', '--workers',   help='Number of workers to use for multi-worker executors (e.g. futures or condor)', dest='workers', type=int, default=8)
     parser.add_option('-s', '--chunksize', help='Chunk size',        dest='chunksize', type=int, default=10000)
     parser.add_option('-m', '--maxchunks', help='Max number of chunks (for testing)',        dest='maxchunks', type=int, default=None)
+    parser.add_option('-j', '--jNVar',     help='make histograms for nth jet variables', dest='jNVar', default=False, action='store_true')
+    parser.add_option('-b', '--jobs',      help='Number of workers to use for condor dask', dest='jobs', type=int, default=1)
+    parser.add_option(      '--hemPeriod', help='HEM period (PreHEM or PostHEM), default includes entire sample',            dest='hemPeriod', type=str, default="")
     options, args = parser.parse_args()
 
     # set output root file
@@ -82,59 +42,21 @@ def main():
     elif trainingKind == "PN":
         # from processors.rootProcessor_varModule import MainProcessor
         from processors.trainFileProcessor import MainProcessor
-    fileset = s.getFileset(sample, True, options.startFile, options.nFiles, mlTraining=True)
     # getting dictionary of files from a sample collection e.g. "2016_QCD, 2016_WJets, 2016_TTJets, 2016_ZJets"
-    outfile = "MyAnalysis_%s_%d" % (sample, options.startFile) if options.condor or options.dask else "trainFile"
-    # get processor args
-    exe_args = {'workers': options.workers, 'schema': processor.TreeMakerSchema}
-    if options.dask:
-        exe_args = use_dask(options.condor,options.workers,options.port)
-        if options.quiet: exe_args['status'] = False
-
-        client = exe_args['client']
-        while len(client.ncores()) < options.mincores:
-            print('Waiting for more cores to spin up, currently there are {0} available...'.format(len(client.ncores())))
-            print('Dask client info ->', client)
-            time.sleep(10)
-
-    print("sample",sample)
-    sf = s.sfGetter(sample,False)
+    fileset = s.getFileset(sample, True, options.startFile, options.nFiles, mlTraining=True)
+    sf = s.sfGetter(sample,True)
     print("scaleFactor = {}".format(sf))
     # run processor
-    output = processor.run_uproot_job(
-        fileset,
-        treename='TreeMaker2/PreSelection',
-        processor_instance=MainProcessor(sample,sf),
-        executor=processor.dask_executor if options.dask else processor.futures_executor,
-        executor_args=exe_args,
-        chunksize=options.chunksize,
-        maxchunks=options.maxchunks,
-    )
+    MainExecutor = processor.futures_executor
+    if options.dask:
+        runProcessWithErrorHandling(fileset,sample,sf,MainExecutor,MainProcessor,options,trainingKind=trainingKind,trainFileProduction=True)
+    else:
+        exe_args = {
+            'workers': options.workers, 
+            'schema': processor.TreeMakerSchema
+        }
+        run_processor(fileset,sample,sf,MainExecutor,MainProcessor,options,exe_args,trainingKind=trainingKind,trainFileProduction=True)
 
-    # saving input to root files.
-    # When inputs are too large, saving the inputs could return an error. That's why we need to save them in batches.
-    haddCommand = "hadd -f trainFile.root "
-    rmCommand = "rm "
-    maxNumOfJets = 100000
-    jVarName = "njetsAK8"
-    if trainingKind == "PN":
-        jVarName = "pT"
-    numOfJets = len(output[jVarName].value)
-    print("numOfJets",numOfJets)
-    sampleName = sample.replace("_TuneCP5_13TeV-madgraphMLM-pythia8","")
-    partNumber = 0
-    for i in range(0,numOfJets,maxNumOfJets):
-        outputNPZ = {}
-        fileName = "tree_{}_M{}_{}.root".format(sampleName,options.startFile,partNumber)
-        partNumber += 1
-        file = up.recreate(fileName)
-        for v in output.keys():
-            try:
-                outputNPZ[v] = output[v].value[i:i+maxNumOfJets]
-            except:
-                outputNPZ[v] = output[v].value[i:-1]
-        file["tree"] = outputNPZ
-        file["tree"].show()
     dt = time.time() - tstart
     print("run time: %.2f [sec]" % (dt))
 
