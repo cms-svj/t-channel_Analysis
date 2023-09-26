8d9d483b4b6b31995d47e086bd598585bcc095e2
Keane-Tan <chin.lung.t93@gmail.com>
2023-09-25 21:36:56 -0500

    Added more scale factors for different scenarios

--- batchRunBackgroundNN.py
+++ batchRunBackgroundNN.py
@@ -2,48 +2,103 @@ from utils import samples as s
 import os
 import sys
 import numpy as np
-
+from glob import glob
 # run this on the lpg-gpu since they have more RAM
 
-# 2018
-reRunDetail = {
-"2018_QCD_Pt_1000to1400": 1,
-"2018_QCD_Pt_1400to1800": 1,
-"2018_QCD_Pt_1800to2400": 1,
-"2018_QCD_Pt_2400to3200": 1,
-"2018_QCD_Pt_300to470": 200,
-"2018_QCD_Pt_3200toInf": 1,
-"2018_QCD_Pt_470to600": 10,
-"2018_QCD_Pt_600to800": 2,
-"2018_QCD_Pt_800to1000": 1,
-"2018_TTJets_DiLept_TuneCP5_13TeV-madgraphMLM-pythia8": 200,
-"2018_TTJets_DiLept_genMET-150_TuneCP5_13TeV-madgraphMLM-pythia8": 10,
-"2018_TTJets_HT-1200to2500_TuneCP5_13TeV-madgraphMLM-pythia8": 2,
-"2018_TTJets_HT-2500toInf_TuneCP5_13TeV-madgraphMLM-pythia8": 1,
-"2018_TTJets_HT-600to800_TuneCP5_13TeV-madgraphMLM-pythia8": 5,
-"2018_TTJets_HT-800to1200_TuneCP5_13TeV-madgraphMLM-pythia8": 3,
-"2018_TTJets_SingleLeptFromT_TuneCP5_13TeV-madgraphMLM-pythia8": 60,
-"2018_TTJets_SingleLeptFromT_genMET-150_TuneCP5_13TeV-madgraphMLM-pythia8": 20,
-"2018_TTJets_SingleLeptFromTbar_TuneCP5_13TeV-madgraphMLM-pythia8": 80,
-"2018_TTJets_SingleLeptFromTbar_genMET-150_TuneCP5_13TeV-madgraphMLM-pythia8": 20,
-"2018_TTJets_Incl_TuneCP5_13TeV-madgraphMLM-pythia8": 290
-}
+def fileExists(existingFiles,fileName):
+    fileExists = False
+    for exFile in existingFiles:
+        if fileName in exFile:
+            fileExists = True
+            break
+    if fileExists == False:
+        print(fileName)
+    return fileExists
 
-batch = 10
+
+tcut = "_pre" # _pre or _pre_1PSVJ
+outHistF = "output/eventTaggerOutput/trainingFiles/wpt0p9{}/".format(tcut)
+resume = True
+
+# 2018
+# pre
+if tcut == "_pre":
+    reRunDetail = {
+    "2018_QCD_Pt_300to470": 200,
+    "2018_QCD_Pt_470to600": 10,
+    "2018_QCD_Pt_600to800": 2,
+    "2018_QCD_Pt_800to1000": 1,
+    "2018_QCD_Pt_1000to1400": 1,
+    "2018_QCD_Pt_1400to1800": 1,
+    "2018_QCD_Pt_1800to2400": 1,
+    "2018_QCD_Pt_2400to3200": 1,
+    "2018_QCD_Pt_3200toInf": 1,
+    "2018_TTJets_DiLept_TuneCP5_13TeV-madgraphMLM-pythia8": 400, # 200
+    "2018_TTJets_DiLept_genMET-150_TuneCP5_13TeV-madgraphMLM-pythia8": 20, # 10
+    "2018_TTJets_HT-600to800_TuneCP5_13TeV-madgraphMLM-pythia8": 10, # 5
+    "2018_TTJets_HT-800to1200_TuneCP5_13TeV-madgraphMLM-pythia8": 6, # 3
+    "2018_TTJets_HT-1200to2500_TuneCP5_13TeV-madgraphMLM-pythia8": 4, # 2
+    "2018_TTJets_HT-2500toInf_TuneCP5_13TeV-madgraphMLM-pythia8": 2, # 1
+    "2018_TTJets_Incl_TuneCP5_13TeV-madgraphMLM-pythia8": 290, # 290
+    "2018_TTJets_SingleLeptFromT_TuneCP5_13TeV-madgraphMLM-pythia8": 120, # 60
+    "2018_TTJets_SingleLeptFromT_genMET-150_TuneCP5_13TeV-madgraphMLM-pythia8": 40, # 20
+    "2018_TTJets_SingleLeptFromTbar_TuneCP5_13TeV-madgraphMLM-pythia8": 160, # 80
+    "2018_TTJets_SingleLeptFromTbar_genMET-150_TuneCP5_13TeV-madgraphMLM-pythia8": 40, # 20
+    }
+    batch = 10
+elif tcut == "_pre_1PSVJ":
+    reRunDetail = {
+    '2018_QCD_Pt_300to470': 200, 
+    '2018_QCD_Pt_470to600': 20, 
+    '2018_QCD_Pt_600to800': 10, 
+    '2018_QCD_Pt_800to1000': 5, 
+    '2018_QCD_Pt_1000to1400': 5, 
+    '2018_QCD_Pt_1400to1800': 5, 
+    '2018_QCD_Pt_2400to3200': 5, 
+    '2018_QCD_Pt_1800to2400': 5,
+    '2018_TTJets_DiLept_TuneCP5_13TeV-madgraphMLM-pythia8': 300,  
+    '2018_TTJets_DiLept_genMET-150_TuneCP5_13TeV-madgraphMLM-pythia8': 100, 
+    '2018_TTJets_HT-600to800_TuneCP5_13TeV-madgraphMLM-pythia8': 50, 
+    '2018_TTJets_HT-800to1200_TuneCP5_13TeV-madgraphMLM-pythia8': 30, 
+    '2018_TTJets_HT-2500toInf_TuneCP5_13TeV-madgraphMLM-pythia8': 10, 
+    '2018_TTJets_HT-1200to2500_TuneCP5_13TeV-madgraphMLM-pythia8': 20, 
+    '2018_TTJets_Incl_TuneCP5_13TeV-madgraphMLM-pythia8': 290, 
+    '2018_TTJets_SingleLeptFromT_TuneCP5_13TeV-madgraphMLM-pythia8': 500, 
+    '2018_TTJets_SingleLeptFromT_genMET-150_TuneCP5_13TeV-madgraphMLM-pythia8': 200, 
+    '2018_TTJets_SingleLeptFromTbar_TuneCP5_13TeV-madgraphMLM-pythia8': 500, 
+    '2018_TTJets_SingleLeptFromTbar_genMET-150_TuneCP5_13TeV-madgraphMLM-pythia8': 200, 
+    }
+    batch = 20
+
+existingFiles = glob("{}/*.root".format(outHistF))
 
 for rerun,num in reRunDetail.items():
     sample = rerun.replace("_TuneCP5_13TeV-madgraphMLM-pythia8","")
+    sampleName = sample.replace("_TuneCP5_13TeV-madgraphMLM-pythia8","")
     if num <= batch:
-        command = "python analyze_root_varModule.py -d {} -N {} -w 4 -s 1000 --training NN".format(sample,num)
-        print(command)
-        os.system(command)
-        command = "mv trainFile.root tree_{}_NN.root".format(sample.replace("2018_",""))
-        os.system(command)
-    else:
-        for mVal in np.arange(0,num,batch):
-            command = "python analyze_root_varModule.py -d {} -N 10 -M {} -w 8 -s 1000 --training NN".format(sample,mVal)
+        fileName = "tree_{}_M0".format(sampleName)
+        if resume:
+            if (fileExists(existingFiles,fileName) == False):
+                command = "python analyze_root_varModule.py -d {} -N {} -w 16 -s 1000 --training NN --outHistF {} --tcut {}".format(sample,num,outHistF,tcut)
+                print(command)
+                os.system(command)
+        else:
+            command = "python analyze_root_varModule.py -d {} -N {} -w 16 -s 1000 --training NN --outHistF {} --tcut {}".format(sample,num,outHistF,tcut)
             print(command)
             os.system(command)
-            command = "mv trainFile.root tree_{}_NN_M{}.root".format(sample.replace("2018_",""),mVal)
-            os.system(command)
+
+    else:
+        for mVal in np.arange(0,num,batch):
+            fileName = "tree_{}_M{}".format(sampleName,mVal)
+            NVal = batch
+            if num - mVal < batch:
+                NVal = num-mVal
+            command = "python analyze_root_varModule.py -d {} -N {} -M {} -w 16 -s 1000 --training NN --outHistF {} --tcut {}".format(sample,NVal,mVal,outHistF,tcut)
+            if resume:
+                if (fileExists(existingFiles,fileName) == False):
+                    print(command)
+                    os.system(command)
         
+            else:
+                print(command)
+                os.system(command)
--- utils/samples.py
+++ utils/samples.py
@@ -2,7 +2,7 @@ from os import system, environ
 import json
 import glob
 
-def getFileset(sample,verbose=True,startFile=0,nFiles=-1,mlTraining=False):
+def getFileset(sample,verbose=False,startFile=0,nFiles=-1,mlTraining=False):
     # find all json files for the sample or sample collection
     f_ = sample.find("_")
     year = sample[:f_]
@@ -26,8 +26,8 @@ def getFileset(sample,verbose=True,startFile=0,nFiles=-1,mlTraining=False):
     inputSamples = glob.glob(JSONDir+"*"+detailKey+"*.json")
     if len(inputSamples) == 0:
         print("Error: no json file found with name:", JSONDir)
-    else:
-        print(inputSamples)
+    # else:
+    #     print(inputSamples)
 
     # open all json files and dump them into a dictionary
     fileset_all = {}
@@ -64,41 +64,88 @@ def getAllFilesets():
     fBG.update(fSG2)
     return fBG
 
-def sfGetter(sample,on=False):
-    # scaleFactor for particleNet training files
+def sfGetter(sample,on=False,tcut=""):
     if on:
-        print("Yes")
-        scaleFactor = {
-                "2018_QCD_Pt_1000to1400": 19730000.0/48000,
-                "2018_QCD_Pt_1400to1800": 10982000.0/21000,
-                "2018_QCD_Pt_1800to2400": 5491000.0/48000,
-                "2018_QCD_Pt_2400to3200": 2997000.0/30000,
-                "2018_QCD_Pt_300to470": 57910000.0/9423000,
-                "2018_QCD_Pt_3200toInf": 1000000.0/51000,
-                "2018_QCD_Pt_470to600": 52448000.0/447000,
-                "2018_QCD_Pt_600to800": 67508000.0/96000,
-                "2018_QCD_Pt_800to1000": 37160000.0/48000,
-                "2018_TTJets_DiLept": 29290487.0/8532037,
-                "2018_TTJets_DiLept_genMET-150": 10592111.0/103384,
-                "2018_TTJets_HT-1200to2500": 2009331.0/29630,
-                "2018_TTJets_HT-2500toInf": 1001084.0/78961,
-                "2018_TTJets_HT-600to800": 15258099.0/83062,
-                "2018_TTJets_HT-800to1200": 9201990.0/69470,
-                "2018_TTJets_SingleLeptFromT": 58237254.0/2572246,
-                "2018_TTJets_SingleLeptFromT_genMET-150": 13337428.0/414361,
-                "2018_TTJets_SingleLeptFromTbar": 58510607.0/3589663,
-                "2018_TTJets_SingleLeptFromTbar_genMET-150": 12597052.0/503985,
-                "2018_TTJets_Incl": 10494353.0/10339641,
-        }
+        # scaleFactor for particleNet/event tagger training files (pre)
+        if tcut == "_pre":
+            scaleFactor = {
+                "2018_QCD_Pt_1000to1400": 19730000/48000.,
+                "2018_QCD_Pt_1400to1800": 10982000/21000.,
+                "2018_QCD_Pt_1800to2400": 5491000/48000.,
+                "2018_QCD_Pt_2400to3200": 2997000/30000.,
+                "2018_QCD_Pt_300to470": 57910000/9423000.,
+                "2018_QCD_Pt_3200toInf": 1000000/3000.,
+                "2018_QCD_Pt_470to600": 52448000/447000.,
+                "2018_QCD_Pt_600to800": 67508000/96000.,
+                "2018_QCD_Pt_800to1000": 37160000/48000.,
+                "2018_TTJets_DiLept": 29290487/16993527.,
+                "2018_TTJets_DiLept_genMET-150": 10592111/195331.,
+                "2018_TTJets_HT-600to800": 15258099/179188.,
+                "2018_TTJets_HT-800to1200": 9201990/135827.,
+                "2018_TTJets_HT-1200to2500": 2009331/70237.,
+                "2018_TTJets_HT-2500toInf": 1001084/40694.,
+                "2018_TTJets_Incl": 10494353/10339641.,
+                "2018_TTJets_SingleLeptFromT": 58237254/5080853.,
+                "2018_TTJets_SingleLeptFromT_genMET-150": 13337428/845757.,
+                "2018_TTJets_SingleLeptFromTbar": 58510607/6941755.,
+                "2018_TTJets_SingleLeptFromTbar_genMET-150": 12597052/968711.,
+                }
+        # scaleFactor for particleNet/event tagger training files (pre)
+        elif tcut == "_pre_1PSVJ":
+            scaleFactor = {
+                "2018_QCD_Pt_1000to1400": 19730000/225000.,
+                "2018_QCD_Pt_1400to1800": 10982000/213000.,
+                "2018_QCD_Pt_1800to2400": 5491000/183000.,
+                "2018_QCD_Pt_2400to3200": 2997000/207000.,
+                "2018_QCD_Pt_300to470": 57910000/9423000.,
+                "2018_QCD_Pt_3200toInf": 1000000/195000.,
+                "2018_QCD_Pt_470to600": 52448000/927000.,
+                "2018_QCD_Pt_600to800": 67508000/480000.,
+                "2018_QCD_Pt_800to1000": 37160000/207000.,
+                "2018_TTJets_DiLept": 29290487/12663839.,
+                "2018_TTJets_DiLept_genMET-150": 10592111/1870456.,
+                "2018_TTJets_HT-1200to2500": 2009331/591459.,
+                "2018_TTJets_HT-2500toInf": 1001084/129330.,
+                "2018_TTJets_HT-600to800": 15258099/974946.,
+                "2018_TTJets_HT-800to1200": 9201990/611853.,
+                "2018_TTJets_SingleLeptFromT": 58237254/19505097.,
+                "2018_TTJets_SingleLeptFromT_genMET-150": 13337428/3905981.,
+                "2018_TTJets_SingleLeptFromTbar": 58510607/18798625.,
+                "2018_TTJets_SingleLeptFromTbar_genMET-150": 12597052/4489650.,
+                "2018_TTJets_Incl": 10494353/10339641.,
+                }
+        # for 20 files per sample, used for small subset of samples to make histograms
+        else:
+            scaleFactor = {
+                "2018_QCD_Pt_80to120": 29685000/1899000.,
+                "2018_QCD_Pt_120to170": 29949000/1824000.,
+                "2018_QCD_Pt_170to300": 29676000/960000.,
+                "2018_QCD_Pt_1000to1400": 19730000/936000.,
+                "2018_QCD_Pt_1400to1800": 10982000/843000.,
+                "2018_QCD_Pt_1800to2400": 5491000/654000.,
+                "2018_QCD_Pt_2400to3200": 2997000/786000.,
+                "2018_QCD_Pt_300to470": 57910000/870000.,
+                "2018_QCD_Pt_3200toInf": 1000000/484000.,
+                "2018_QCD_Pt_470to600": 52448000/927000.,
+                "2018_QCD_Pt_600to800": 67508000/960000.,
+                "2018_QCD_Pt_800to1000": 37160000/906000.,
+                "2018_TTJets_DiLept": 29290487/871719.,
+                "2018_TTJets_DiLept_genMET-150": 10592111/195331.,
+                "2018_TTJets_HT-1200to2500": 2009331/591459.,
+                "2018_TTJets_HT-2500toInf": 1001084/184038.,
+                "2018_TTJets_HT-600to800": 15258099/345290.,
+                "2018_TTJets_HT-800to1200": 9201990/392841.,
+                "2018_TTJets_SingleLeptFromT": 58237254/835004.,
+                "2018_TTJets_SingleLeptFromT_genMET-150": 13337428/414361.,
+                "2018_TTJets_SingleLeptFromTbar": 58510607/863322.,
+                "2018_TTJets_SingleLeptFromTbar_genMET-150": 12597052/503985.,
+                "2018_TTJets_Incl": 10494353/672958.
+                }
         sf = 1
         for key,item in scaleFactor.items():
             if key == sample:
-                print("Yes")
                 sf = item
                 break
     else:
         sf = 1
     return sf
-
-
-
