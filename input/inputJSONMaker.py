db0a983b8084527278e844d6793c0a5d398cf460
Keane-Tan <chin.lung.t93@gmail.com>
2024-08-06 10:14:05 -0500

    Able to update/create input JSON for skims now

--- input/inputJSONMaker.py
+++ input/inputJSONMaker.py
@@ -2,18 +2,14 @@
 # create json files that store dictionary of input files of different samples
 import json
 import os
-from inputDictionary import yearDict,backgrounds,signals,data,postpendToRemove
 
-skimSource = False
+skimSource = True
 if skimSource:
-    tmNtpLocation = ""
-    tmSigLocation = ""
-    ntupleKind = "skims"
+    from inputDictionary_skim import backgrounds,signals,data
 else:
-    tmNtpLocation = "/store/user/lpcsusyhad/SusyRA2Analysis2015/Run2ProductionV20"
-    tmSigLocation = "/store/user/lpcdarkqcd/tchannel_UL"
-    ntupleKind = "treeMakerNtuples"
+    from inputDictionary import yearDict,backgrounds,signals,data,postpendToRemove
 
+skimModule = "t_channel_pre_selection"
 
 def getEosContent(eosSource):
     os.system(f"eosls {eosSource} > out.txt")
@@ -37,37 +33,60 @@ def makeJSONFiles(sampleDict):
     sampleLabelList = []
     for sampleGroup, sampleList in sampleDict.items():
         year = sampleGroup[:sampleGroup.find("_")]
+        if skimSource:
+            skimLocation = f"/store/user/lpcdarkqcd/tchannel_UL/{year}/Full/PrivateSkims/nominal/{year}/{skimModule}/"
+            ntupleKind = "skims"
+        else:
+            dataBkgLocation = "/store/user/lpcsusyhad/SusyRA2Analysis2015/Run2ProductionV20"
+            sigLocation = "/store/user/lpcdarkqcd/tchannel_UL"
+            ntupleKind = "treeMakerNtuples"
         print(year)
         sampleGroupLabelList.append(sampleGroup)
-        if "Data" in sampleGroup:
-            yearAliasList = yearDict[year]["data"]
-        elif "SVJ" in sampleGroup: # signal
-            yearAliasList = [year]
+        if skimSource:
+            outputDir = f"sampleJSONs/{ntupleKind}/{skimModule}/{sampleGroup}"
         else:
-            yearAliasList = yearDict[year]["background"]
-        outputDir = f"sampleJSONs/{ntupleKind}/{sampleGroup}"
+            if "Data" in sampleGroup:
+                yearAliasList = yearDict[year]["data"]
+            elif "SVJ" in sampleGroup: # signal
+                yearAliasList = [year]
+            else:
+                yearAliasList = yearDict[year]["background"]
+            outputDir = f"sampleJSONs/{ntupleKind}/{sampleGroup}"
         os.makedirs(outputDir,exist_ok=True)
         for sampleName in sampleList:
             print(sampleName)
-            sampleFileName = f"{year}_{removePostpend(sampleName,sampleGroup)}"
-            allSampleLocationList = []
-            for yearAlias in yearAliasList:
-                if "SVJ" in sampleGroup: # signal
-                    sampleGroupLocation = f"{tmSigLocation}/{yearAlias}/Full/PrivateSamples"
-                else:
-                    sampleGroupLocation = f"{tmNtpLocation}/{yearAlias}"
-                sampleNameList = getEosContent(sampleGroupLocation)
-                if sampleName in sampleNameList:
-                    sampleFileList = getEosContent(f"{sampleGroupLocation}/{sampleName}")
-                    print(f"{len(sampleFileList)} files")
-                    sampleLocationList = [f"root://cmseos.fnal.gov/{tmNtpLocation}/{yearAlias}/{sampleName}/{sampleFile}" for sampleFile in sampleFileList]
-                    allSampleLocationList += sampleLocationList
-            print(f"{outputDir}/{sampleFileName}.json")
-            sampleLabelList.append(sampleFileName)
-            print()
+            if skimSource:
+                sampleFileName = f"{year}_{sampleName}"
+                if sampleName == "TTJets":
+                    sampleFileName += "_Incl"
+                elif "SVJ" in sampleGroup:
+                    sampleFileName = sampleFileName.replace("t-channel_","")
+                sampleFileList = getEosContent(f"{skimLocation}/{sampleName}")
+                sampleLocationList = [f"root://cmseos.fnal.gov/{skimLocation}/{sampleName}/{sampleFile}" for sampleFile in sampleFileList]
+                print(f"{outputDir}/{sampleFileName}.json")
+                with open(f"{outputDir}/{sampleFileName}.json","w") as fp:
+                    json.dump({sampleFileName:sampleLocationList}, fp, indent=4)
 
-            with open(f"{outputDir}/{sampleFileName}.json","w") as fp:
-                json.dump({sampleFileName:allSampleLocationList}, fp, indent=4)
+            else:
+                sampleFileName = f"{year}_{removePostpend(sampleName,sampleGroup)}"
+                allSampleLocationList = []
+                for yearAlias in yearAliasList:
+                    if "SVJ" in sampleGroup: # signal
+                        sampleGroupLocation = f"{sigLocation}/{yearAlias}/Full/PrivateSamples"
+                    else:
+                        sampleGroupLocation = f"{dataBkgLocation}/{yearAlias}"
+                    sampleNameList = getEosContent(sampleGroupLocation)
+                    if sampleName in sampleNameList:
+                        sampleFileList = getEosContent(f"{sampleGroupLocation}/{sampleName}")
+                        print(f"{len(sampleFileList)} files")
+                        sampleLocationList = [f"root://cmseos.fnal.gov/{dataBkgLocation}/{yearAlias}/{sampleName}/{sampleFile}" for sampleFile in sampleFileList]
+                        allSampleLocationList += sampleLocationList
+                print(f"{outputDir}/{sampleFileName}.json")
+                sampleLabelList.append(sampleFileName)
+                print()
+
+                with open(f"{outputDir}/{sampleFileName}.json","w") as fp:
+                    json.dump({sampleFileName:allSampleLocationList}, fp, indent=4)
 
 makeJSONFiles(backgrounds)
 makeJSONFiles(data)
