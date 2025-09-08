fixed quick bug with scaling dnn score

utils/runEventTagger.py
@@ -141,24 +141,24 @@ def normalize(evtTaggerDict, varVal):
     df_target_std = pd.DataFrame(df_target_std, columns=branches_to_keep)
     return df_target_std
 
-# inferencing model
+
 def runEventTagger(events, varVal, skimSource, evtTaggerDict):
     def _minmax_scale(arr, eps=1e-6):
-    arr = np.asarray(arr, dtype=float)
-    mask = np.isfinite(arr)
+        arr = np.asarray(arr, dtype=float)
+        mask = np.isfinite(arr)
 
-    if not np.any(mask):
-        return np.zeros_like(arr)
+        if not np.any(mask):
+            return np.zeros_like(arr)
 
-    a_min, a_max = arr[mask].min(), arr[mask].max()
-    if a_max > a_min:
-        arr[mask] = np.clip((arr[mask] - a_min) / (a_max - a_min), 0, 1-eps)
-    else:
-        arr[mask] = 0.0  # all values identical
+        a_min, a_max = arr[mask].min(), arr[mask].max()
+        if a_max > a_min:
+            arr[mask] = np.clip((arr[mask] - a_min) / (a_max - a_min), 0, 1-eps)
+        else:
+            arr[mask] = 0.0  # all values identical
 
-    arr[~mask] = 0.0  # replace NaN/Inf with 0
-    return arr
-
+        arr[~mask] = 0.0  # replace NaN/Inf with 0
+        return arr
+    # inferencing model
     evtTaggerVars(events, varVal, skimSource, evtTaggerDict)
     df_target_std = normalize(evtTaggerDict, varVal)
     model = evtTaggerDict["model"]
@@ -168,7 +168,8 @@ def runEventTagger(events, varVal, skimSource, evtTaggerDict):
     features = opt_training_options.features_training
     event_tagger_score = model(torch.tensor(df_target_std[features].values, dtype=torch.float32))
     #varVal["dnnEventClassScore"] = event_tagger_score.detach().numpy().flatten()
-    # ✅ apply min-max scaling
+
+    # apply min-max scaling
     scaled_event_tagger_score = _minmax_scale(event_tagger_score.detach().numpy().flatten())
     varVal["dnnEventClassScore"] = scaled_event_tagger_score
     