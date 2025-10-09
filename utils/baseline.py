03ceb9415c70eb5a0ed1e7dd6fd86c03d3ec0051
Akshat Shriavstava <akshriva@ttu.edu>
2025-10-09 12:05:11 -0500

    fixed minor bug

--- utils/baseline.py
+++ utils/baseline.py
@@ -324,9 +324,9 @@ def cutList(dataset,events,vars_noCut,hemStudy,trgEffStudy,hemPeriod,skimCut,ski
             "_pre_WNAE_0SVJ":                        preselection & (nsvjWNAE == 0),
             "_pre_WNAE_1SVJ":                        preselection & (nsvjWNAE == 1),
             "_pre_WNAE_2SVJ":                        preselection & (nsvjWNAE == 2),
-            "_pre_WNAE_2PSVJ"                        preselection & (nsvjWNAE >= 2),
+            "_pre_WNAE_2PSVJ":                        preselection & (nsvjWNAE >= 2),
             "_pre_WNAE_3SVJ":                        preselection & (nsvjWNAE == 3),
-            "_pre_WNAE_3PSVJ"                        preselection & (nsvjWNAE >= 3),
+            "_pre_WNAE_3PSVJ":                        preselection & (nsvjWNAE >= 3),
             "_pre_WNAE_4PSVJ":                       preselection & (nsvjWNAE >= 4),
 
 
