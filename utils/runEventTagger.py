fb107b19dc3ca9ab18ec1abc92c0a8daa10e653e
Akshat Shriavstava <akshriva@ttu.edu>
2025-10-23 21:03:46 -0500

    temperarly turned off global min max as model does not need it

--- utils/runEventTagger.py
+++ utils/runEventTagger.py
@@ -178,8 +178,8 @@ def runEventTagger(events, varVal, skimSource, evtTaggerDict):
     opt_training_options = locate(config_training_options)
     features = opt_training_options.features_training
     event_tagger_score = model(torch.tensor(df_target_std[features].values, dtype=torch.float32))
-    #varVal["dnnEventClassScore"] = event_tagger_score.detach().numpy().flatten()
-    scores = event_tagger_score.detach().numpy().flatten()
-    scaled_scores = _minmax_scale(scores,gmin, gmax)
-    varVal["dnnEventClassScore"] = scaled_scores
+    varVal["dnnEventClassScore"] = event_tagger_score.detach().numpy().flatten()
+    # scores = event_tagger_score.detach().numpy().flatten()
+    # scaled_scores = _minmax_scale(scores,gmin, gmax)
+    # varVal["dnnEventClassScore"] = scaled_scores
     
